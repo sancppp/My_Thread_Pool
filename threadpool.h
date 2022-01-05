f8c091a2f98c2202d58ba108df8dfe17140fcbe8
sancppp <2096067752@qq.com>
2022-01-05 23:25:03 +0800

    init

---
+++ threadpool.h
@@ -0,0 +1,23 @@
+#ifndef THREAD_POOL_H_
+#define THREAD_POOL_H_
+#include <thread>
+#include <mutex>
+
+class ThreadPool
+{
+private:
+    /* data */
+public:
+    ThreadPool(/* args */);
+    ~ThreadPool();
+};
+
+ThreadPool::ThreadPool(/* args */)
+{
+}
+
+ThreadPool::~ThreadPool()
+{
+}
+
+#endif
