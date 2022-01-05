bcc514b98b4490833975a2647ca2d6975bd82b1b
sancppp <2096067752@qq.com>
2022-01-05 22:25:31 +0800

    add

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+//-----pre_def----
+const double PI = acos(-1.0);
+const int INF = 0x3f3f3f3f;
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef pair<int, int> PII;
+typedef pair<double, double> PDD;
+#define IOS ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+#define fir(i, a, b) for (int i = (a); i <= (b); i++)
+#define rif(i, a, b) for (int i = (a); i >= (b); i--)
+#define endl '\n'
+#define init_h memset(h, -1, sizeof h), idx = 0;
+#define lowbit(x) x &(-x)
+
+//---------------
+const int N = 1e5+10;
+int n, m, d[N];
+
+void init(){}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    int StartTime = clock();
+#endif
+    int T = 1;
+    scanf("%d",&T);
+    while (T--)
+    {
+    }
+#ifndef ONLINE_JUDGE
+    printf("Run_Time = %d ms\n", clock() - StartTime);
+#endif
+    return 0;
+}
