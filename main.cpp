ae1136d07f5e17fdaef368a500c07c4560d25d26
sancppp <2096067752@qq.com>
2022-01-06 19:45:58 +0800

    second

--- main.cpp
+++ main.cpp
@@ -1,25 +1,7 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <ctime>
 using namespace std;
-//-----pre_def----
-const double PI = acos(-1.0);
-const int INF = 0x3f3f3f3f;
-typedef long long LL;
-typedef unsigned long long ULL;
-typedef pair<int, int> PII;
-typedef pair<double, double> PDD;
-#define IOS ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
-#define fir(i, a, b) for (int i = (a); i <= (b); i++)
-#define rif(i, a, b) for (int i = (a); i >= (b); i--)
-#define endl '\n'
-#define init_h memset(h, -1, sizeof h), idx = 0;
-#define lowbit(x) x &(-x)
-
-//---------------
-const int N = 1e5+10;
-int n, m, d[N];
-
-void init(){}
+void init() {}
 int main()
 {
 #ifndef ONLINE_JUDGE
@@ -28,10 +10,10 @@ int main()
     int StartTime = clock();
 #endif
     int T = 1;
-    scanf("%d",&T);
+    scanf("%d", &T);
     while (T--)
     {
-        cout<<"hello world"<<endl;
+        cout << "hello world" << endl;
     }
 #ifndef ONLINE_JUDGE
     printf("Run_Time = %d ms\n", clock() - StartTime);
