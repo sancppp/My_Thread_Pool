587e961be31ccb777370b9da6a0fc50dd10518e9
sancppp <2096067752@qq.com>
2022-01-06 07:13:09 -0500

    linux init

--- main.cpp
+++ main.cpp
@@ -1,22 +1,15 @@
 #include <iostream>
 #include <ctime>
 using namespace std;
-void init() {}
 int main()
 {
-#ifndef ONLINE_JUDGE
-    freopen("in.txt", "r", stdin);
-    freopen("out.txt", "w", stdout);
-    int StartTime = clock();
-#endif
+
     int T = 1;
     scanf("%d", &T);
     while (T--)
     {
         cout << "hello world" << endl;
     }
-#ifndef ONLINE_JUDGE
-    printf("Run_Time = %d ms\n", clock() - StartTime);
-#endif
+
     return 0;
 }
